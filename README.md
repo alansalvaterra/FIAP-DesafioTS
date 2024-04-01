576b9f31a8cecdacd5ac8e08cdd846acd8cbc8e4
alansalvaterra <alan.salvaterra@gmail.com>
2024-04-01 00:39:35 -0300

    Testando

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ A API oferece as seguintes rotas:
 
 ## Tecnologias utilizadas:
 
+- Node.js: Ambiente de execução de código JavaScript do lado do servidor, amplamente utilizado para criar aplicações web e API REST.
 - TypeScript: Linguagem de programação utilizada para o desenvolvimento do projeto.
 - TypeORM: Biblioteca de ORM utilizada para interagir com o banco de dados SQL.
 - SQLite: Banco de dados SQL utilizado para armazenar os dados dos livros.
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,15 @@ app.use(express.json());
 
 // Rota de boas-vindas
 app.get('/', (req, res) => {
-    res.send('Bem-vindo à API de gerenciamento de livros!');
+    res.send(`Bem-vindo à API de gerenciamento de livros do Desafio Pos Tech - TypeScript!<br><br>
+    A API oferece as seguintes rotas:<br><br>
+
+    - GET /books: Retorna todos os livros cadastrados.<br>
+    - GET /books/:id: Retorna os detalhes de um livro específico pelo seu ID.<br>
+    - POST /books: Adiciona um novo livro.<br>
+    - PUT /books/:id: Atualiza os detalhes de um livro existente.<br>
+    - DELETE /books/:id: Exclui um livro existente.<br>
+    `);
 });
 
 // Rotas relacionadas aos livros (./Routes/books.ts)
