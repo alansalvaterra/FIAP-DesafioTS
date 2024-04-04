84fa9ce5547a7fc3bea76ec0954d3c60d5e9e617
alansalvaterra <alan.salvaterra@gmail.com>
2024-04-04 00:22:42 -0300

    Criando relacionamento e interface IPublisher.ts

---
+++ src/app/interfaces/IPublisher.ts
@@ -0,0 +1,6 @@
+interface IPublisher {
+    id?: number;
+    name: string;
+}
+
+export default IPublisher;
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Book } from "../app/entities/Book"
+import { Publisher } from "../app/entities/Publisher"
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "database.sqlite",
     synchronize: true,
     logging: false,
-    entities: [Book],
+    entities: [Book, Publisher],
     migrations: [],
     subscribers: [],
 })
